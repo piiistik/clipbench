193a992b5673fb9aa730fadf08ad19fef6323d22
stefan.vasak <stefan.vasak@thermofisher.com>
2025-12-04 10:59:56 +0100

    first prototype version

--- clipex/core/command_runner/command_runner.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import List
-
-
-class CommandRunner(ABC):
-    @abstractmethod
-    def run(self, commands: List[str]) -> List[float]: ...
--- clipex/ui/cli/cli.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from CLI 👋")
-
-
-if __name__ == "__main__":
-    main()
